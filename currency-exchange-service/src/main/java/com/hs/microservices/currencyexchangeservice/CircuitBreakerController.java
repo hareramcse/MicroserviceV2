a6375117d09b60cac2f440b914d217e6a03172cc
Hareram Singh <hareramcse@gmail.com>
2023-01-11 16:29:11 +0530

    Adding RateLimiter and BulkHead

--- currency-exchange-service/src/main/java/com/hs/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/hs/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -2,12 +2,10 @@ package com.hs.microservices.currencyexchangeservice;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @RestController
 public class CircuitBreakerController {
@@ -15,12 +13,15 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping("/sample-api")
-	//@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
-	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	// @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	// @CircuitBreaker(name = "sample-api", fallbackMethod = "hardcodedResponse")
+	// @RateLimiter(name="sample-api")
+	@Bulkhead(name = "sample-api") // this is for concurrent calls
 	public String sampleApi() {
 		logger.info("Sample api call received");
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
-				String.class);
+		// ResponseEntity<String> forEntity = new
+		// RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+		// String.class);
 		return "sample-api";
 	}
 
