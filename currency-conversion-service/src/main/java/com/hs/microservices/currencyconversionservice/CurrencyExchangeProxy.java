c6b3d0457f15e9f1d9e01fc3b394b987b8708e62
Hareram Singh <hareramcse@gmail.com>
2023-01-11 14:31:25 +0530

    Adding naming server and feign client to distribute the load on running instances.

--- currency-conversion-service/src/main/java/com/hs/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/hs/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
-//@FeignClient(name="currency-exchange")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name="currency-exchange")// it will check all the instances of the currency-exchange in euereka and load balance the request
 public interface CurrencyExchangeProxy {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
